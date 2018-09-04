7540adf2be9992ecc4c6004c57af383f74613d82
Aleksandar Budić <aleksandar.budic@nultien.rs>
2018-09-04 12:40:02 +0200

    removed duplicate test cases from RoverTests

--- MarsRoverTests/RoverTests.cs
+++ MarsRoverTests/RoverTests.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using MarsRover;
+﻿using MarsRover;
 using MarsRover.Positioning;
 using Xunit;
 
@@ -24,74 +22,6 @@ namespace MarsRoverTests
             var rover = new Rover(position);
 
             Assert.Equal(position, rover.Position);
-        }
-
-        [Theory]
-        [MemberData(nameof(RoverMovesForwardCases))]
-        public void RoverMovesForward(Position startingPosition, Position positionAfterMovement)
-        {
-            var rover = new Rover(startingPosition);
-
-            rover.MoveForward();
-
-            Assert.Equal(positionAfterMovement, rover.Position);
-        }
-
-        [Theory]
-        [MemberData(nameof(RoverMovesBackwardsCases))]
-        public void RoverMovesBackwards(Position startingPosition, Position positionAfterMovement)
-        {
-            var rover = new Rover(startingPosition);
-
-            rover.MoveBackwards();
-
-            Assert.Equal(positionAfterMovement, rover.Position);
-        }
-
-        [Theory]
-        [InlineData(DirectionEnum.North, DirectionEnum.East)]
-        [InlineData(DirectionEnum.East, DirectionEnum.South)]
-        [InlineData(DirectionEnum.South, DirectionEnum.West)]
-        [InlineData(DirectionEnum.West, DirectionEnum.North)]
-        public void RoverTurnsRight(DirectionEnum startingDirection, DirectionEnum directionAfterTurn)
-        {
-            var position = new Position(0, 0, startingDirection);
-            var rover = new Rover(position);
-
-            rover.TurnRight();
-
-            Assert.Equal(directionAfterTurn, rover.Position.Direction.Value);
-        }
-
-        [Theory]
-        [InlineData(DirectionEnum.North, DirectionEnum.West)]
-        [InlineData(DirectionEnum.West, DirectionEnum.South)]
-        [InlineData(DirectionEnum.South, DirectionEnum.East)]
-        [InlineData(DirectionEnum.East, DirectionEnum.North)]
-        public void RoverTurnsLeft(DirectionEnum startingDirection, DirectionEnum directionAfterTurn)
-        {
-            var position = new Position(0, 0, startingDirection);
-            var rover = new Rover(position);
-
-            rover.TurnLeft();
-
-            Assert.Equal(directionAfterTurn, rover.Position.Direction.Value);
-        }
-
-        public static IEnumerable<object[]> RoverMovesForwardCases()
-        {
-            yield return new object[] { new Position(0, 0, DirectionEnum.North), new Position(0, 1, DirectionEnum.North) };
-            yield return new object[] { new Position(0, 1, DirectionEnum.South), new Position(0, 0, DirectionEnum.South) };
-            yield return new object[] { new Position(0, 0, DirectionEnum.East), new Position(1, 0, DirectionEnum.East) };
-            yield return new object[] { new Position(1, 0, DirectionEnum.West), new Position(0, 0, DirectionEnum.West) };
-        }
-
-        public static IEnumerable<object[]> RoverMovesBackwardsCases()
-        {
-            yield return new object[] { new Position(0, 1, DirectionEnum.North), new Position(0, 0, DirectionEnum.North) };
-            yield return new object[] { new Position(0, 0, DirectionEnum.South), new Position(0, 1, DirectionEnum.South) };
-            yield return new object[] { new Position(1, 0, DirectionEnum.East), new Position(0, 0, DirectionEnum.East) };
-            yield return new object[] { new Position(0, 0, DirectionEnum.West), new Position(1, 0, DirectionEnum.West) };
         }
     }
 }
